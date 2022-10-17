a86d8ebde6d6d1b2b234bcdd8b2da9a2994b10ce
RyutoOzawa <k021g1163@g.neec.ac.jp>
2022-10-17 11:10:03 +0900

    01_04

--- PG3_01_01/main.cpp
+++ PG3_01_01/main.cpp
@@ -2,43 +2,60 @@
 #include<Windows.h>
 #include<time.h>
 #include<stdlib.h>
+#include<functional>
 
-typedef void (*Pfunc)();
+typedef int (*Pfunc)(int);
 
 //コールバック関数
-void Callback() {
+int Dice(int num) {
 	//ランダムな値を返す
-	printf("結果:%d\n",rand());
+	int randNum = rand();
+	randNum  %= 6 + 1;
+	printf("さいころの結果:%d\n",randNum);
+	
+	if (num % 2 == randNum % 2) {
+		printf("プレイヤーの勝ちです");
+	}
+	else {
+		printf("プレイヤーの負けです");
+	}
+	return randNum;
+
 }
 
-void SetTimeOut(Pfunc p, int second) {
+void SetTimeOut(Pfunc p, int second,int num) {
 	//引数秒待機
 	printf("待機中...\n");
 	Sleep(second * 1000);
 	//コールバック関数の呼び出し
-	p();
+	p(num);
 }
 
-int main() {
+int main(int argc,const char *argv[]) {
 	
 	srand(time(nullptr));
 	
 	
 	Pfunc p;
-	p = Callback;
-	printf("値を入力して下さい\n");
+	p = Dice;
 	int num = 0;
-	scanf_s("%d", &num);
-
-	SetTimeOut(p, 3);
-
-	//結果が偶数の場合
-	if (num %2 ==0) {
-		printf("入力された値は偶数です\n");
+	printf("奇数だと思うならば1,偶数だと思うならば2を押してください\n");
+	while (true) {
+		scanf_s("%d", &num);
+		//入力値が2か1ならループ終わり
+		if (num == 2 || num == 1) {
+			break;
+		}
+		else {
+			//設定値以外ならエラーメッセージ
+			printf("入力値エラー:1か2を入力してください\n");
+		}
 	}
-	else {
-		printf("入力された値は奇数です\n");
-	}
+
+	std::function<void(Pfunc,int,int)> fx = [](Pfunc p,int second,int num){
+		SetTimeOut(p, second,num);
+	};
+	fx(p, 3,num);
 
 
 	return 0;
