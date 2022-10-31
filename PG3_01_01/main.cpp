25bbc4b6756ca7a14e9d1d43b7c93de146740799
RyutoOzawa <k021g1163@g.neec.ac.jp>
2022-10-31 10:45:12 +0900

    02_01完成

--- PG3_01_01/main.cpp
+++ PG3_01_01/main.cpp
@@ -2,55 +2,59 @@
 #include<Windows.h>
 #include<time.h>
 #include<stdlib.h>
+#include<list>
+#include<string>
+#include<iostream>
+using namespace std;
 
-typedef void (*Pfunc)(int);
 
-//コールバック関数
-void Dice(int num) {
-	//ランダムな値を返す
-	int randNum;
-	randNum = rand() % 6 + 1;
-	
-	printf("さいころの結果:%d\n", randNum);
-	if (randNum % 2 == num % 2) {
-		printf("プレイヤーの勝ち\n");
-	}
-	else {
-		printf("プレイヤーの負け");
-	}
-	
-}
-
-void SetTimeOut(Pfunc p, int second,int num) {
-	//引数秒待機
-	printf("待機中...\n");
-	Sleep(second * 1000);
-	//コールバック関数の呼び出し
-	p(num);
-}
 
 int main() {
-	
-	srand(time(nullptr));
-	
-	
-	Pfunc p;
-	p = Dice;
-	int num = 0;
-	printf("奇数だと思うならば1,偶数だと思うならば2を押してください;\n");
-	while (true) {
-		scanf_s("%d", &num);
-		if (num == 1 || num == 2) {
-			break;
-		}
-		else {
-			printf("入力値エラー:1か2を入力してください");
-		}
 
+	
+
+	list<string> yamanoteLine{
+		"Tokyo","Kanda","Akihabara","Okachimachi","Ueno","Uguisudani","Nippori","Tabata","Komagome",
+		"Sugamo","Otsuka","Ikebukuro","Mejiro","Takadanobaba","Shin-Okubo","Shinjuku","Yoyogi","Harajuku",
+		"Shibuya","Ebisu","Meguro","Gotanda","Osaki","Shinagawa","Tamachi","Hamamatsucho","Shimbashi","Yurakucho"};
+
+	list<string>::iterator itr = yamanoteLine.begin();
+	
+	//1970の駅一覧
+	printf("1970年の山手線一覧\n");
+	for (auto i = yamanoteLine.begin(); i != yamanoteLine.end(); i++) {
+		cout << *i << "\n";
+		
 	}
-	SetTimeOut(p, 3,num);
 
+	for (list<string>::iterator i = yamanoteLine.begin(); i != yamanoteLine.end(); i++) {
+		//田端の前に西日暮里を追加
+		if (*i == "Tabata") {
+			i = yamanoteLine.insert(i, "Nishi-Nippori");
+			++i;
+		}
+	}
 
+	//2019の駅一覧
+	printf("2019年の山手線一覧\n");
+	for (auto i = yamanoteLine.begin(); i != yamanoteLine.end(); i++) {
+		cout << *i << "\n";
+		
+	}
+
+	for (list<string>::iterator i = yamanoteLine.begin(); i != yamanoteLine.end(); i++) {
+		//田町の前に高輪ゲートウェイを追加
+		if (*i == "Tamachi") {
+			i = yamanoteLine.insert(i, "Takanawa Gateway");
+			++i;
+		}
+	}
+
+	//2022の駅一覧
+	printf("2022年の山手線一覧\n");
+	for (auto i = yamanoteLine.begin(); i != yamanoteLine.end(); i++) {
+		cout << *i << "\n";
+	}
 
 	return 0;
 }
