24ededfe9387fe3ac3f2db3a59c1f25e2baa15b9
RyutoOzawa <k021g1163@g.neec.ac.jp>
2022-11-21 11:31:26 +0900

    未完成

--- PG3_01_01/main.cpp
+++ PG3_01_01/main.cpp
@@ -1,35 +1,20 @@
 #include<stdio.h>
+#include"SceneManager.h"
 
-template <typename t>
-t Min(t a, t b) {
-	if (a > b) {
-		return b;
-	}
-	return a;
-}
-
-template <>
-char Min<char>(char a, char b) {
-	return static_cast<char>('0');
-}
+enum sceneIndex {
+	Title,
+	NewGame,
+	GamePlay,
+	GameClear,
+};
 
 int main() {
+	SceneManager* sceneManager = SceneManager::GetInstance();
+	int nextNum = 0;
 
-	int intX = 4;
-	int intY = 8;
-	float floatX = 1.5f;
-	float floatY = 2.3f;
-	double doubleX = 1.1412;
-	double doubleY = 2.4325;
-	char charX = 'c';
-	char charY = 'a';
 
-	printf("%d\n", Min<int>(intX, intY));
-	printf("%f\n", Min<float>(floatX, floatY));
-	printf("%lf\n", Min<double>(doubleX, doubleY));
-	if (Min(charX, charY) == '0') {
-		printf("”’l‚Ì‘ã“ü‚Í‚Å‚«‚Ü‚¹‚ñ\n");
-	}
 
+	printf("nowScene:%d\n", sceneManager->sceneNo);
+	
 	return 0;
 }
