a12605ce41b26596cab674831f90f6c6298a26b3
RyutoOzawa <k021g1163@g.neec.ac.jp>
2022-12-02 16:07:45 +0900

    双方向無理！

--- PG3_01_01/List.h
+++ PG3_01_01/List.h
@@ -1,5 +1,73 @@
 #pragma once
+#include<vector>
+#include<string>
+
+struct CELL {
+	char data[8];
+	CELL* prev = nullptr;
+	CELL* next = nullptr;
+};
+
+
 class List
 {
+public:
+	//リストの生成
+	void Create(CELL* currentCell, const char* data) {
+		CELL* newCell;
+		newCell = (CELL*)malloc(sizeof(CELL));
+		strcpy_s( newCell->data ,8, data);
+		newCell->prev = currentCell;
+		newCell->next = currentCell->next;
+
+		//最後尾に挿入できるようにする
+		if (currentCell->next) {
+			CELL* newCell = currentCell->next;
+			newCell->prev = newCell;
+		}
+
+		currentCell->next = newCell;
+
+	};
+
+	//セル一覧表示
+	void Index(CELL* endCell) {
+		int no = 1;
+		while (endCell->next != nullptr) {
+	
+			endCell = endCell->next;
+			printf("%d:", no);
+			printf("%s,\n", endCell->data);
+			no++;
+		}
+		printf("\n");
+		printf("要素数:%d\n", no);
+	};
+
+	//任意の位置のセルのアドレスを取得
+	CELL* GetInsertCellAdress(CELL* cell, int iterator) {
+		//セルをイテレータの数だけ進める
+		for (int i = 0; i < iterator; i++) {
+			if (cell->next) {
+				cell = cell->next;
+			}
+			else {
+				break;
+			}
+		}
+		return cell;
+	}
+
+	int GetAllCell(CELL* cell) {
+		int allnum = 0;
+		CELL* insertCell = cell;
+		while (insertCell->next != nullptr)
+		{
+			insertCell = insertCell->next;
+			allnum++;
+		}
+
+		return allnum;
+	}
 };
 
--- PG3_01_01/main.cpp
+++ PG3_01_01/main.cpp
@@ -1,20 +1,148 @@
 #include<stdio.h>
-#include"SceneManager.h"
+#include"List.h"
 
-enum sceneIndex {
-	Title,
-	NewGame,
-	GamePlay,
-	GameClear,
+
+enum ListState {
+	MENU,	//0,メニュー
+	DISPLAY,//1,要素表示
+	INSERT,	//2,挿入
+	EDIT,	//3,編集
+	DELETE,	//4,削除
+	SORT,	//5,並び替え
 };
 
 int main() {
-	SceneManager* sceneManager = SceneManager::GetInstance();
-	int nextNum = 0;
+	int inputNum = 0;
+	int allCellCount = 0;
+	CELL* cell = new CELL;
+	cell->next = nullptr;
+	List list;
 
-
-
-	printf("nowScene:%d\n", sceneManager->sceneNo);
+	strcpy_s(cell->data, 6, "aiueo");
+	list.Create(cell, "abcde");
+	list.Create(cell, "aaaaa");
+	list.Create(cell, "bbbbb");
+	list.Create(cell, "ccccc");
 	
+
+	while (1)
+	{
+		//入力された番号によって表示変更
+		if (inputNum == MENU) {
+
+			printf("[要素の操作]\n");
+			printf("1.要素の表示\n");
+			printf("2.要素の挿入\n");
+			printf("3.要素の編集\n");
+
+			//要素が1以上の時のみ表示
+			if (cell) {
+				printf("4.要素の削除\n");
+				printf("5.要素の並び替え(オプション)\n");
+			}
+
+			printf("\n");
+			printf("-----------------------------\n");
+			printf("操作を選択してください\n");
+			scanf_s("%d", &inputNum);
+
+		}
+		else if (inputNum == DISPLAY) {
+			int disprayInput = 0;
+			printf("[要素の表示]\n");
+			printf("1.要素の一覧表示\n");
+			printf("2.要素を指定して要素を表示\n");
+			printf("9.要素操作に戻る\n");
+			printf("\n");
+			printf("操作を選択してください\n");
+			scanf_s("%d", &disprayInput);
+			while (disprayInput != 1 && disprayInput != 2 && disprayInput != 9) {
+				printf("入力エラー:操作を入力してください\n");
+				scanf_s("%d", &disprayInput);
+			}
+			if (disprayInput == 1) {
+				printf("[要素の一覧表示]\n");
+				printf("要素一覧:{\n");
+				//要素の一覧表示をここに
+				list.Index(cell);
+
+				printf("\n");
+				printf("-------------------------\n");
+				printf("1.要素の表示に戻る\n");
+				printf("2.要素の操作に戻る\n");
+				scanf_s("%d", &inputNum);
+				if (inputNum == 1) {
+					inputNum = DISPLAY;
+				}
+				else if (inputNum == 2) {
+					inputNum = MENU;
+				}
+
+			}
+			else if (disprayInput == 2) {
+				printf("[順番を指定して要素を表示]\n");
+				printf("表示したい要素の順番を指定してください\n");
+				int datanum = 0;
+				scanf_s("%d", &datanum);
+				//データ番号使って要素を表示
+				//要素の数を調べる
+				allCellCount = list.GetAllCell(cell);
+
+
+				printf("\n");
+				if (datanum < allCellCount) {
+					CELL* displayCell = cell;
+					for (int i = 0; i < allCellCount; i++) {
+						displayCell = displayCell->next;
+					}
+					printf("%d : %s\n", allCellCount, displayCell->data);
+				}
+				else {
+					printf("指定要素が範囲外です");
+				}
+
+				printf("\n");
+				printf("-------------------------\n");
+				printf("1.要素の表示に戻る\n");
+				printf("2.要素の操作に戻る\n");
+				scanf_s("%d", &inputNum);
+
+			}
+			else if (disprayInput == 9) {
+				inputNum = MENU;
+			}
+
+
+		}
+		else if (inputNum == EDIT) {
+			printf("[要素の編集]\n");
+			printf("編集したい要素の番号を指定してください\n");
+			int editInput = 0;
+			scanf_s("%d\n" ,&editInput);
+
+			//要素がある場合
+			if(true){}
+			//要素がない場合
+			else {}
+
+			//メニューに戻る
+			inputNum = 0;
+		}
+		else if (inputNum == INSERT) {
+			printf("[要素の挿入]\n");
+		}
+		else if (inputNum == DELETE) {
+			printf("[要素の削除]\n");
+		}
+		else if (inputNum == SORT) {
+			printf("[要素の並び替え]\n");
+		}
+		else {
+			printf("入力エラー");
+			inputNum = 0;
+		}
+	}
+
+
 	return 0;
 }
