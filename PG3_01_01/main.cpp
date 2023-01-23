9466c4ebbd415cdb9f138272302cc1f9b9c9a6e2
RyutoOzawa <k021g1163@g.neec.ac.jp>
2023-01-23 11:17:04 +0900

    03_01完了

--- PG3_01_01/main.cpp
+++ PG3_01_01/main.cpp
@@ -10,24 +10,41 @@ enum sceneIndex {
 
 int main() {
 	SceneManager* sceneManager = SceneManager::GetInstance();
-	int nextNum = 0;
+	int nextScene = 0;
 
-	printf("1を押すと次のシーンへ移動します。\n");
-	if (sceneManager->sceneNo == Title) {
-		printf("現在のシーン:タイトル\n");
-	}
-	else if (sceneManager->sceneNo == NewGame) {
-		printf("現在のシーン:ニューゲーム\n");
-	}
-	else if (sceneManager->sceneNo == GamePlay) {
-		printf("現在のシーン:げーむぷれい\n");
-	}
-	else if (sceneManager->sceneNo == GameClear) {
-		printf("現在のシーン:タイトル\n");
-	}
+	int input = 0;
 
+	while (true) {
 
-	printf("nowScene:%d\n", sceneManager->sceneNo);
+		printf("1を押すと次のシーンへ移動します。\n");
+		scanf_s("%d", &input);
+		if (input == 1) {
+			nextScene = sceneManager->sceneNo;
+			nextScene++;
+			if (nextScene > GameClear) {
+				nextScene = Title;
+			}
+			sceneManager->ChangeScene(nextScene);
+		}
+		else {
+			printf("シーンは移動しませんでした\n");
+		}
+
+		if (sceneManager->sceneNo == Title) {
+			printf("現在のシーン:タイトル\n");
+		}
+		else if (sceneManager->sceneNo == NewGame) {
+			printf("現在のシーン:ニューゲーム\n");
+		}
+		else if (sceneManager->sceneNo == GamePlay) {
+			printf("現在のシーン:ゲームプレイ\n");
+		}
+		else if (sceneManager->sceneNo == GameClear) {
+			printf("現在のシーン:ゲームクリア\n");
+		}
+
+	}
+	//printf("nowScene:%d\n", sceneManager->sceneNo);
 	
 	return 0;
 }
