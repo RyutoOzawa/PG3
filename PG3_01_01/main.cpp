ad0be85bec49620972ae47fe57eee4d308253650
RyutoOzawa <k021g1163@g.neec.ac.jp>
2023-01-23 10:45:41 +0900

    途中

--- PG3_01_01/main.cpp
+++ PG3_01_01/main.cpp
@@ -12,6 +12,19 @@ int main() {
 	SceneManager* sceneManager = SceneManager::GetInstance();
 	int nextNum = 0;
 
+	printf("1を押すと次のシーンへ移動します。\n");
+	if (sceneManager->sceneNo == Title) {
+		printf("現在のシーン:タイトル\n");
+	}
+	else if (sceneManager->sceneNo == NewGame) {
+		printf("現在のシーン:ニューゲーム\n");
+	}
+	else if (sceneManager->sceneNo == GamePlay) {
+		printf("現在のシーン:げーむぷれい\n");
+	}
+	else if (sceneManager->sceneNo == GameClear) {
+		printf("現在のシーン:タイトル\n");
+	}
 
 
 	printf("nowScene:%d\n", sceneManager->sceneNo);
